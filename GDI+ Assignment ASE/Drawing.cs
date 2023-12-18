dbbbf5a28d852ed71395bca7b2980f807b0aa8b3
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-18 11:15:40 +0000

    Created a new drawing,.cs file woth same code as it was in the properties folder.Basically made a mistake in storing the file in a proper folder.

--- GDI+ Assignment ASE/Drawing.cs
+++ GDI+ Assignment ASE/Drawing.cs
@@ -10,19 +10,19 @@ namespace GDI__Assignment_ASE.Properties
 {
     public class Drawing
     {
-        public void draw(Graphics g,String command)
+        public void draw(Graphics g, String command)
         {
             Pen pen = new Pen(Color.Red, 2);
 
             if (command == "circle")
             {
-            Circle circle = new Circle(25);
-            circle.draw(g, pen);
+                Circle circle = new Circle(25);
+                circle.draw(g, pen);
             }
 
-            else if (command == "square" || command  == "rectangle")
+            else if (command == "square" || command == "rectangle")
             {
-                rectangle rect = new rectangle(50,50);
+                rectangle rect = new rectangle(50, 50);
                 rect.draw(g, pen);
             }
         }
