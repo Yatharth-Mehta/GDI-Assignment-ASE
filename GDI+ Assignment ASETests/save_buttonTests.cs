63a2d07e9afe833a7831ff63050828bbf61f0360
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-31 22:26:55 +0000

    Created a Unit test for save_button.cs that will check the if the object sender is null.

---
+++ GDI+ Assignment ASETests/save_buttonTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class save_buttonTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void save_buttonTest()
+        {
+            save_button sb = new save_button(null);
+        }
+    }
+}
