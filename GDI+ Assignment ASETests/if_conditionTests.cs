e8445d97d24c13dd5ec36f5e749655a193eddd87
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-31 20:53:53 +0000

    Created a Unit test for if_condition.cs that will check the if the graphic g variable is null.

---
+++ GDI+ Assignment ASETests/if_conditionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class if_conditionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void if_statementTest()
+        {
+            if_condition ifc = new if_condition(null, null);
+            ifc.if_statement();
+        }
+    }
+}
---
+++ GDI+ Assignment ASETests/if_condition_callingTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class if_condition_callingTests
+    {
+        [TestMethod()]
+        public void cmdsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
