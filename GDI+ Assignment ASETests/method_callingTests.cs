4d89e0f806cd165e808f8742ebbdd6513523db25
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-31 21:25:27 +0000

    Created a Unit test for method_calling.cs that will check the if the graphic g variable is null.

---
+++ GDI+ Assignment ASETests/method_callingTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class method_callingTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void cmdsTest()
+        {
+            method_calling mc = new method_calling(null,null,null,null,null);
+            mc.cmds();
+        }
+    }
+}
