eaffcd915cc7f99fbc5e414dcc0c4421893bc45e
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-31 20:36:42 +0000

    Created a Unit test for clear.cs that will check the if the graphic g variable is null.

---
+++ GDI+ Assignment ASETests/clearTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class clearTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void drawTest()
+        {
+            clear c = new clear();
+            c.draw(null);
+        }
+    }
+}
