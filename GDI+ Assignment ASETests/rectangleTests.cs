c7a41f1a94a48d3317a23eb1bcacaadaaa3676e7
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-31 21:34:16 +0000

    Created a Unit test for rectangle.cs that will check the if the graphic g variable is null.

---
+++ GDI+ Assignment ASETests/rectangleTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class rectangleTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void drawTest()
+        {
+            rectangle r = new rectangle(50,90);
+            r.draw(null, null, 0, 0);
+        }
+    }
+}
