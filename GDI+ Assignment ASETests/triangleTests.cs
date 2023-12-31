e386ebc437be934be75601ffe641dda3bbcd07c0
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2023-12-31 21:42:58 +0000

    Created a Unit test for triangle.cs that will check the if the graphic g variable is null.

---
+++ GDI+ Assignment ASETests/triangleTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class triangleTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void drawTest()
+        {
+            triangle tr = new triangle(null,null);
+            tr.draw(null,null,0,0);
+
+        }
+    }
+}
