797a6df82d4dcc4dc80374f9e84c87ccff7e8611
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-01 03:13:14 +0000

    XML documentation for triangle.cs and draw and drawfill method

--- GDI+ Assignment ASE/triangle.cs
+++ GDI+ Assignment ASE/triangle.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is Triangle class that will draw a triangle with the co-ordinates given by the user.
+    /// </summary>
     public class triangle
     {
+        /// <summary>
+        /// This constructor will take the graphic object and pen object so drawing can be posiibel further in the class.
+        /// </summary>
+        /// <param name="g">The Graphic object</param>
+        /// <param name="p">The Pen object</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public triangle(Graphics g,Pen p)
         {
             if (g is null)
@@ -17,6 +26,20 @@ namespace GDI__Assignment_ASE
             }
         }
 
+        /// <summary>
+        /// This method will draw a non-fillled triangle with the points of co-ordinates entered by the user
+        /// </summary>
+        /// <param name="g">Graphic object to draw a triangle</param>
+        /// <param name="p">Pen object in order to draw a triangle</param>
+        /// <param name="xpos">x-position on the canvas where the recctangle is to be drawn</param>
+        /// <param name="ypos">y-position on the canvas where the rectangle is to be drawn</param>
+        /// <param name="a1">The x value of first point of the triangle</param>
+        /// <param name="a2">The y value of first point of the triangle</param>
+        /// <param name="b1">The x value of second point of the triangle</param>
+        /// <param name="b2">The y value of second point of the triangle</param>
+        /// <param name="c1">The x value of third point of the triangle</param>
+        /// <param name="c2">The y value of third point of the triangle</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public void draw(Graphics g, Pen p, int xpos, int ypos,int a1,int a2,int b1,int b2,int c1,int c2)
         {
             if (g is null)
@@ -27,6 +50,20 @@ namespace GDI__Assignment_ASE
             g.DrawPolygon(p, points);
         }
 
+        /// <summary>
+        /// This method will draw a fillled triangle with the points of co-ordinates entered by the user
+        /// </summary>
+        /// <param name="g">Graphic object to draw a triangle</param>
+        /// <param name="brush">Brush object in order to draw a triangle</param>
+        /// <param name="xpos">x-position on the canvas where the recctangle is to be drawn</param>
+        /// <param name="ypos">y-position on the canvas where the rectangle is to be drawn</param>
+        /// <param name="a1">The x value of first point of the triangle</param>
+        /// <param name="a2">The y value of first point of the triangle</param>
+        /// <param name="b1">The x value of second point of the triangle</param>
+        /// <param name="b2">The y value of second point of the triangle</param>
+        /// <param name="c1">The x value of third point of the triangle</param>
+        /// <param name="c2">The y value of third point of the triangle</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public void drawfill(Graphics g, Brush brush, int xpos, int ypos, int a1, int a2, int b1, int b2, int c1, int c2)
         {
             if (g is null)
