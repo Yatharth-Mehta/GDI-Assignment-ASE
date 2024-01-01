b3853ae41a3523dc9afe927010423cd1d7550711
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-01 02:46:56 +0000

    XML documentation for clear.cs and draw method

--- GDI+ Assignment ASE/clear.cs
+++ GDI+ Assignment ASE/clear.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is Clear class that will clear the canvas whenever called.
+    /// </summary>
     public class clear
     {
+        /// <summary>
+        /// This draw method will clear the whole canvas and will give it the color white.
+        /// </summary>
+        /// <param name="g">Graphics object used to clear</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public void draw(Graphics g)
         {
             if (g is null)
