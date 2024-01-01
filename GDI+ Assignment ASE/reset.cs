42a512bb3ab5d894fca872b2f2e43db0b799e3ae
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-01 03:05:55 +0000

    XML documentation for reset.cs and do_reset method

--- GDI+ Assignment ASE/reset.cs
+++ GDI+ Assignment ASE/reset.cs
@@ -7,8 +7,17 @@ using System.Windows.Forms;
 
 namespace GDI__Assignment_ASE
 {
-    public class reset 
+    /// <summary>
+    /// This class will reset the whole form by clearing the multiline and commandline box , drawing canvas etc.
+    /// </summary>
+    public class reset
     {
+        /// <summary>
+        /// This constructor will access the commandline and multiline to reset those.
+        /// </summary>
+        /// <param name="commandline">The string in commandline</param>
+        /// <param name="multiline">The string in multiline</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the commandline or multiline variable is null</exception>
         public reset(String commandline , String multiline)
         {
             if (commandline is null)
@@ -23,6 +32,10 @@ namespace GDI__Assignment_ASE
             multiline = "";
         }
 
+        /// <summary>
+        /// This will actually reset the target element when called as it returns blank space.
+        /// </summary>
+        /// <returns></returns>
         public String do_reset()
         {
             return " ";
