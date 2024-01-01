c7d2897fcc12398842cfa87108d8647dfd187f5f
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-01 02:22:55 +0000

    XML documentation for circle.cs and draw and drawfill method

--- GDI+ Assignment ASE/circle.cs
+++ GDI+ Assignment ASE/circle.cs
@@ -8,15 +8,29 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is Circle class that will draw a circle with the radius given by the user.
+    /// </summary>
     public class Circle
     {
         private int radius;
-
+        /// <summary>
+        /// A constructor is made that will take the radius entered and will assign it to the local variable which is used to 
+        /// draw a circle.
+        /// </summary>
+        /// <param name="radius">The radius variable essential to draw the circle</param>
         public Circle(int radius)
         {
             this.radius = radius;
         }
-
+        /// <summary>
+        /// This method will draw the non-filled cirle.
+        /// </summary>
+        /// <param name="g">Graphic variable to draw a circle</param>
+        /// <param name="p">Pen to draw a circle</param>
+        /// <param name="xpos">x-position on the canvas where the circle is to be drawn</param>
+        /// <param name="ypos">y-position on the canvas where the circle is to be drawn</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public void draw(Graphics g, Pen p,int xpos ,int ypos)
         {
             if (g is null)
@@ -25,6 +39,14 @@ namespace GDI__Assignment_ASE
             }
             g.DrawEllipse(p, xpos,ypos, radius, radius);
         }
+        /// <summary>
+        /// This method will draw the a filled cirle.
+        /// </summary>
+        /// <param name="g">Graphic variable to draw a circle</param>
+        /// <param name="brush">Pen to draw a circle</param>
+        /// <param name="xpos">x-position on the canvas where the circle is to be drawn</param>
+        /// <param name="ypos">y-position on the canvas where the circle is to be drawn</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public void drawfill(Graphics g, Brush brush, int xpos, int ypos)
         {
             if (g is null)
