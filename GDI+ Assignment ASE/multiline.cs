1a3ae99f8434bb2d58c655a052db423ba417724d
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-01 01:44:39 +0000

    XML documentation for multililne.cs and multiline_main method

--- GDI+ Assignment ASE/multiline.cs
+++ GDI+ Assignment ASE/multiline.cs
@@ -11,7 +11,10 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace GDI__Assignment_ASE
-{
+{/// <summary>
+/// This is a class for multiline box commands where the commands passed 
+/// in the multiline textbox will be checked one by one and will be executed.
+/// </summary>
     public class multiline
     {
         String code;
@@ -24,13 +27,26 @@ namespace GDI__Assignment_ASE
         List<string> method_commands= new List<string>();
         Graphics g;
 
+        /// <summary>
+        /// A constructor is created which will take value of fill , the whole code written in the multiline box and the Graphics
+        /// to draw and will assign to the global class variables.
+        /// </summary>
+        /// <param name="code">The whole multiline box text</param>
+        /// <param name="g">The graphic variabe needed to draw</param>
+        /// <param name="fillvalue">The fillvalue in order to know whetheer the shape is to be drawn with
+        /// fill or without fill</param>
         public multiline(String code, Graphics g, String fillvalue)
         {
             this.code = code;
             this.g = g;
             this.fillvalue = fillvalue;
         }
-     
+
+     /// <summary>
+     /// This is the main method which will check the conditions and will run the commands by calling their respective class
+     /// </summary>
+     /// <returns></returns>
+     /// <exception cref="ArgumentNullException"></exception>
         public string variables()
         {
             if (g is null)
@@ -786,270 +802,3 @@ namespace GDI__Assignment_ASE
         }
     }
 }
-
-
-
-/*for (int j = i + 1; j < lines.Length; j++)
-                    {
-                        if (lines[j].Trim() == "endif")
-                        {
-                            Font f = new Font("Arial", 14);
-                            g.DrawString("Hello there", f, Brushes.Red, new Point(50, 50));
-                        }
-                            String[] for_loop_command = lines[j].Split(' ');
-                            if (for_loop_command[0] == "circle")
-                            {
-                                if (vars.Contains(for_loop_command[1]))
-                                {
-                                    Font f = new Font("Arial", 14);
-                                    g.DrawString("obiwan", f, Brushes.Red, new Point(50, 50));
-
-                                }
-                            }
-                        
-                    }*/
-
-
-/*  
-                                            String[] split_command_if = lines[j].Split(' ');
-                                            if (commandlist.Contains(split_command_if[0]))
-                                            {
-                                                split_command_if[0].ToLower();
-                                                switch (split_command_if[0])
-                                                {
-                                                    case "circle":
-                                                        if (int.TryParse(split_command_if[1], out _))
-                                                        {
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, lines[j], fillvalue);
-                                                        }
-                                                        else if (vars.Contains(split_command_if[1]))
-                                                        {
-                                                            int _element = Array.IndexOf(vars, split_command_if[1]);
-                                                            String circle_command = split_command_if[0] + " " + values[_element];
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, circle_command, fillvalue);
-                                                        }
-                                                        break;
-
-                                                    case "rectangle":
-                                                        if (int.TryParse(split_command_if[1], out _) && int.TryParse(split_command_if[2], out _))
-                                                        {
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, lines[j], fillvalue);
-                                                        }
-                                                        else if (vars.Contains(split_command_if[1]) && vars.Contains(split_command_if[2]))
-                                                        {
-                                                            int _element = Array.IndexOf(vars, split_command_if[1]);
-                                                            int element2 = Array.IndexOf(vars, split_command_if[2]);
-                                                            String rect_command = split_command_if[0] + " " + values[_element] + " " + values[element2];
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, rect_command, fillvalue);
-                                                        }
-                                                        break;
-
-                                                    case "square":
-                                                        if (int.TryParse(split_command_if[1], out _) && int.TryParse(split_command_if[2], out _))
-                                                        {
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, lines[j], fillvalue);
-                                                        }
-                                                        else if (vars.Contains(split_command_if[1]) && vars.Contains(split_command_if[2]))
-                                                        {
-                                                            int _element = Array.IndexOf(vars, split_command_if[1]);
-                                                            int element2 = Array.IndexOf(vars, split_command_if[2]);
-                                                            String square_command = split_command_if[0] + " " + values[_element] + " " + values[element2];
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, square_command, fillvalue);
-                                                        }
-                                                        break;
-
-                                                    case "clear":
-                                                        if (lines[j] == "clear")
-                                                        {
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, lines[j], fillvalue);
-                                                        }
-                                                        break;
-
-                                                    case "drawto":
-                                                        if (int.TryParse(split_command_if[1], out _) && int.TryParse(split_command_if[2], out _))
-                                                        {
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, lines[j], fillvalue);
-                                                        }
-                                                        else if (vars.Contains(split_command_if[1]) && vars.Contains(split_command_if[2]))
-                                                        {
-                                                            int _element = Array.IndexOf(vars, split_command_if[1]);
-                                                            int element2 = Array.IndexOf(vars, split_command_if[2]);
-                                                            String drawto = split_command_if[0] + " " + values[_element] + " " + values[element2];
-                                                            Drawing d = new Drawing();
-                                                            d.draw(g, drawto, fillvalue);
-                                                        }
-                                                        break;
-                                                    default: break;
-                                                }
-                                            }
-
-                                            if (split_command_if[0].Equals("print"))
-                                            {
-                                                
-                                                if (split_command_if.Length == 2 && vars.Contains(split_command_if[1]))
-                                                {
-                                                    int elem = Array.IndexOf(vars, split_command_if[1]);
-                                                    if (elem >= 0)
-                                                    {
-                                                        Font f = new Font("Arial", 14);
-                                                        g.DrawString(values[elem], f, Brushes.Red, new Point(50, 50));
-                                                    }
-                                                }
-                                                else if (split_command_if.Length == 4 && vars.Contains(split_command_if[1]) && vars.Contains(split_command_if[3]) && (split_command_if[2] == "+" || split_command_if[2] == "-" || split_command_if[2] == "*" || split_command_if[2] == "/"))
-                                                {
-                                                    int variable1 = Array.IndexOf(vars, split_command_if[1]);
-                                                    int variable2 = Array.IndexOf(vars, split_command_if[3]);
-                                                    int value1 = int.Parse(values[variable1]);
-                                                    int value2 = int.Parse(values[variable2]);
-
-                                                    if (split_command_if[2] == "+")
-                                                    {
-                                                        int result = value1 + value2;
-                                                        Font f = new Font("Arial", 14);
-                                                        g.DrawString(result.ToString(), f, Brushes.Red, new Point(50, 50));
-                                                    }
-                                                    if (split_command_if[2] == "-")
-                                                    {
-                                                        int result = value1 - value2;
-                                                        Font f = new Font("Arial", 14);
-                                                        g.DrawString(result.ToString(), f, Brushes.Red, new Point(50, 50));
-                                                    }
-                                                    if (split_command_if[2] == "*")
-                                                    {
-                                                        int result = value1 * value2;
-                                                        Font f = new Font("Arial", 14);
-                                                        g.DrawString(result.ToString(), f, Brushes.Red, new Point(50, 50));
-                                                    }
-                                                    if (split_command_if[2] == "/")
-                                                    {
-                                                        int result = value1 / value2;
-                                                        Font f = new Font("Arial", 14);
-                                                        g.DrawString(result.ToString(), f, Brushes.Red, new Point(50, 50));
-                                                    }
-                                                }
-                                            }
-                                        */
-
-
-
-
-
-
-
-
-
-
-
-
-/*if(vars.Contains(split_command[1]) && vars.Contains(split_command[3]))
-                { 
-                        int elem1 = vars.IndexOf(split_command[1]);
-                        int elem2 = vars.IndexOf(split_command[3]);
-                        Font f = new Font("Arial", 14);
-                        int a = int.Parse(values[elem1]);
-                        int b = int.Parse(values[elem2]);
-
-
-                        if (condition == "==")
-                        {
-                            if (a == b)
-                            {
-                                loop_run = "yes";
-                            }
-                        }
-
-                    }
-                    *//* else if (vars.Contains(split_command[1]) || vars.Contains(split_command[3]))
-                      {
-                          Font f = new Font("Arial", 14);
-                          g.DrawString("Only one is a variable", f, Brushes.Red, new Point(50, 50));
-                      }
-                      else
-                      {
-                          Font f = new Font("Arial", 14);
-                          g.DrawString("None are variables", f, Brushes.Red, new Point(50, 50));
-                      }*//*
-                }
-                if (loop_run == "yes")
-                {
-                    Font f = new Font("Arial", 14);
-                    g.DrawString(lines[i], f, Brushes.Red, new Point(50, 50));
-                }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*while_loop wl = new while_loop(split_command[2]);
-String condition = wl.validation();
-String loop_run = "no";
-if (condition == "false")
-{
-    Font f = new Font("Arial", 14);
-    g.DrawString("Invalid While condition", f, Brushes.Red, new Point(50, 50));
-}
-
-if (condition != "false")
-{
-    if (vars.Contains(split_command[1]))
-    {
-        int elem = vars.IndexOf(split_command[1]);
-        if (int.TryParse(split_command[3], out _))
-        {
-            if (int.Parse(values[elem]) == int.Parse(split_command[3]))
-            {
-                i++;
-                Font f = new Font("Arial", 14);
-                while (lines[i] != "endloop")
-                {
-                    String[] for_commands = lines[i].Split(' ');
-                    if (for_commands[0] == "circle")
-                    {
-                        while_calling wc = new while_calling(lines[i], g, fillvalue);
-                        wc.cmds();
-                        *//*int element = vars.IndexOf(for_commands[1]);
-                        String circle_command = for_commands[0] + " " + values[element];
-                        Drawing d = new Drawing();
-                        d.draw(g, circle_command, fillvalue);*//*
-
-                    }
-
-
-                    i++;
-                }
-            }
-        }
-    }
-}*/
