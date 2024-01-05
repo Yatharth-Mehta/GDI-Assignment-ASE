be3e60cbb5f0f00452a4388539a1c815792257d8
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-05 01:18:42 +0000

    XML Documentation for the if_condtition.cs , it's constructor and it's function.

--- GDI+ Assignment ASE/if_condition.cs
+++ GDI+ Assignment ASE/if_condition.cs
@@ -7,11 +7,22 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is the class to check the condition for "if" condition.
+    /// </summary>
     public class if_condition
     {
+
         String condition;
         Char[] characters;
         Graphics g;
+        /// <summary>
+        /// This constructor will take Graphics element for exception purposes and the condition in 
+        /// string value to check.
+        /// </summary>
+        /// <param name="g">The graphics element</param>
+        /// <param name="condition">The if condition to be checked</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public if_condition(Graphics g, String condition)
         {
             if (g is null)
@@ -21,6 +32,11 @@ namespace GDI__Assignment_ASE
             this.condition = condition;
             this.g = g;
         }
+        /// <summary>
+        /// This method contains a list of all possible if conditions and it will check the passed condition in the list
+        /// and return the condition as String if the value exists in the list.
+        /// </summary>
+        /// <returns>Returns the String value of the ccondition or returns false if the condition is not present in the list</returns>
         public String if_statement()
         {
             List<String> list = new List<String>();
@@ -30,7 +46,6 @@ namespace GDI__Assignment_ASE
             list.Add(">");
             list.Add("<");
             list.Add("!=");
-            //String op = characters[1].ToString() + characters[2].ToString(); 
             if (list.Contains(condition))
             {
                 return condition;
