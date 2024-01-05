bd42c7696151cb108c4d2a3caa8c9892a0168f58
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-05 02:33:12 +0000

    XML documentation of while_loop.cs , it's constructor and it's method.

--- GDI+ Assignment ASE/while_loop.cs
+++ GDI+ Assignment ASE/while_loop.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This class will check whether the condition of while loop is correct or not
+    /// </summary>
     public class while_loop
     {
+
         String condition;
+        /// <summary>
+        /// This constructor will take the condition and check whether it's null or not
+        /// </summary>
+        /// <param name="condition">The condtion passed in the while loop</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public while_loop(string condition)
         {
             if (condition is null)
@@ -17,6 +26,12 @@ namespace GDI__Assignment_ASE
             }
             this.condition = condition;
         }
+        /// <summary>
+        /// This will contain a list of all possible values of the condition and it will check if the 
+        /// condition is correct.
+        /// </summary>
+        /// <returns>It will return the string format of the condition if the condition is present in the list and will return false
+        /// if the condition is not in the list.</returns>
         public String validation()
         {
             List<String> list = new List<String>();
