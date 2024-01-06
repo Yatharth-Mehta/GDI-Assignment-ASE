c22637ce3af1af1096a4bbc33ba949f4b1638174
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:23:39 +0000

    Created a unit test for reset exception that will pass null for graphic object

---
+++ GDI+ Assignment ASETests/reset_exceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class reset_exceptionTests
+    {
+        [TestMethod()]
+        public void reset_exceptionTest()
+        {
+            reset_exception re = new reset_exception(null,0); 
+        }
+    }
+}
