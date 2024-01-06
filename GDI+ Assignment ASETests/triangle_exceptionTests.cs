1dfce1e129b6167c6dcfac577da04534f9eb7df6
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:27:36 +0000

    Created a unit test for triangle exception that will pass null for graphic objecct

---
+++ GDI+ Assignment ASETests/triangle_exceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class triangle_exceptionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+
+        public void triangle_exceptionTest()
+        {
+            triangle_exception te = new triangle_exception(null,0);
+        }
+    }
+}
