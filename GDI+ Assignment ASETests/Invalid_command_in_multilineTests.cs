e76116c8be31b8ddd16954e64c5a1e52d2df9ddb
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:02:12 +0000

    XML documentation for Invalid_command_in_multiline exception class unit test

--- GDI+ Assignment ASETests/Invalid_command_in_multilineTests.cs
+++ GDI+ Assignment ASETests/Invalid_command_in_multilineTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a unit test for Invalid_command_in_multiline exception class. 
+    /// </summary>
     [TestClass()]
     public class Invalid_command_in_multilineTests
     {
+        /// <summary>
+        /// This passes null for Graphic object which will throw an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void Invalid_command_in_multilineTest()
