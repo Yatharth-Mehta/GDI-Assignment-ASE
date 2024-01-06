3747af69074eccc75f61b5dca065a6f7e17f34fd
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 23:50:21 +0000

    XML documentation for circle_exception.cs

--- GDI+ Assignment ASE/circle_exception.cs
+++ GDI+ Assignment ASE/circle_exception.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is an custom exception class for circle command.
+    /// </summary>
     public class circle_exception : Exception
     {
+        /// <summary>
+        /// This constructor will throw the exception.
+        /// </summary>
+        /// <param name="g">Graphic element for drawing string on canvas</param>
+        /// <param name="ypos">The position of the text on Y-axis</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public circle_exception(Graphics g,int ypos)
         {
             if (g is null)
