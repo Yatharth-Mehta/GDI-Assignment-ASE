142effee9fe2f16cb84e5314fe249b2ea52ed5c8
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:19:37 +0000

    This is a unit test for crectangle_square_exception class.

--- GDI+ Assignment ASE/rectangle_sqaure_exception.cs
+++ GDI+ Assignment ASE/rectangle_sqaure_exception.cs
@@ -11,6 +11,10 @@ namespace GDI__Assignment_ASE
     {
         public rectangle_sqaure_exception(Graphics g,int ypos)
         {
+            if (g is null)
+            {
+                throw new ArgumentNullException(nameof(g));
+            }
             Font f = new Font("Arial", 14);
             g.DrawString("Give proper parameters to Rectangle !", f, Brushes.Red, new Point(0, ypos));
         }
---
+++ GDI+ Assignment ASETests/rectangle_sqaure_exceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class rectangle_sqaure_exceptionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+
+        public void rectangle_sqaure_exceptionTest()
+        {
+            rectangle_sqaure_exception rse = new rectangle_sqaure_exception(null, 0);
+        }
+    }
+}
