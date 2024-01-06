00fa2b68043ebb7c742ca0299dc5061b1bdfc69c
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:40:41 +0000

    Created a unit test for while_exception that will pass null for graphic objecct

---
+++ GDI+ Assignment ASETests/while_exceptionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class while_exceptionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+        public void while_exceptionTest()
+        {
+            while_exception we = new while_exception(null, 0);
+        }
+    }
+}
