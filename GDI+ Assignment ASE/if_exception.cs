bb5697b082f1b59014a60deed78f2ff998578351
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 23:59:01 +0000

    XML documentation for if_exception.cs

--- GDI+ Assignment ASE/if_exception.cs
+++ GDI+ Assignment ASE/if_exception.cs
@@ -9,8 +9,17 @@ namespace GDI__Assignment_ASE
 {
     public class if_exception : Exception
     {
+        /// <summary>
+        /// This is an custom exception class for if command.
+        /// </summary>
         public if_exception(Graphics g,int ypos) 
         {
+            /// <summary>
+            /// This constructor will throw an exception by drawing an error string message on the canvas.
+            /// </summary>
+            /// <param name="g">Graphic element for drawing string on canvas</param>
+            /// <param name="ypos">The position of the text on Y-axis</param>
+            /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
             if (g is null)
             {
                 throw new ArgumentNullException(nameof(g));
