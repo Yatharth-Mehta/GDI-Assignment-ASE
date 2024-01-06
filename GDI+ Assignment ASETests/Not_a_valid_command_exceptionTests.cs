d74989c471dff09b66a46ea763e9b02e2d2162f4
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:12:25 +0000

    Created a unit test for Not_a_valid_exception exception that will pass null for graphic objecct

---
+++ GDI+ Assignment ASETests/Not_a_valid_command_exceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDI__Assignment_ASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDI__Assignment_ASE.Tests
+{
+    [TestClass()]
+    public class Not_a_valid_command_exceptionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
+
+        public void Not_a_valid_command_exceptionTest()
+        {
+            Not_a_valid_command_exception nave = new Not_a_valid_command_exception(null, 0);
+        }
+    }
+}
