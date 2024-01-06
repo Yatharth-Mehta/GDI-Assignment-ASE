1ed2881bfdcdfbe821815b940bdf370a78ac7c46
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-06 04:28:13 +0000

    XML documentation for triangle_exception unit test

--- GDI+ Assignment ASETests/triangle_exceptionTests.cs
+++ GDI+ Assignment ASETests/triangle_exceptionTests.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a unit test for cmdline_empty_exception class. 
+    /// </summary>
     [TestClass()]
     public class triangle_exceptionTests
     {
@@ -16,6 +19,9 @@ namespace GDI__Assignment_ASE.Tests
 
         public void triangle_exceptionTest()
         {
+            /// <summary>
+            /// This passes null for Graphic object which will throw an exception.
+            /// </summary>
             triangle_exception te = new triangle_exception(null,0);
         }
     }
