4477d4c05cb4263898e65924f0ffcb797c1cde0d
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:53:09 +0000

    XML documentation method_calling_with_parametres class Unit Test

--- GDI+ Assignment ASETests/method_calling_with_parametresTests.cs
+++ GDI+ Assignment ASETests/method_calling_with_parametresTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for method calling with parametres class which is used to call method that has parametres in it.
+    /// </summary>
     [TestClass()]
     public class method_calling_with_parametresTests
     {
+        /// <summary>
+        /// In this method method_calling_with_parametres class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void cmdsTest()
