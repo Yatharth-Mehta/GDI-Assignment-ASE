2410fb28cb46a02e4d1ceabfc3fdb15492138631
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:46:12 +0000

    XML documentation for if condition Unit Test

--- GDI+ Assignment ASETests/if_conditionTests.cs
+++ GDI+ Assignment ASETests/if_conditionTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for if_condition class which is used to check the if condition user passes.
+    /// </summary>
     [TestClass()]
     public class if_conditionTests
     {
+        /// <summary>
+        /// In this method the if condition class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void if_statementTest()
