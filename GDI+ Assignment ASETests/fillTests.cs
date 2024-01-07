1edd51be9449ceb77c3f74b2798dc450684039fd
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:34:21 +0000

    XML documentation of Fill class Unit Test

--- GDI+ Assignment ASETests/fillTests.cs
+++ GDI+ Assignment ASETests/fillTests.cs
@@ -8,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for fill class which is used to decide the value of fill which will decide whether
+    /// to draw a shape filled or non-filled.
+    /// </summary>
     [TestClass()]
     public class fillTests
     {
+        /// <summary>
+        /// In this method the fill class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void fillTest()
