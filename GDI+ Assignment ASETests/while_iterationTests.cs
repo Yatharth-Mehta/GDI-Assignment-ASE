681dcb7a405e3de240c7bcb6c465a28ab331be50
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 05:31:11 +0000

    XML doccumentation for while_iteration class Unit Test

--- GDI+ Assignment ASETests/while_iterationTests.cs
+++ GDI+ Assignment ASETests/while_iterationTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for while_iteration class which is used for iterating while loop.
+    /// </summary>
     [TestClass()]
     public class while_iterationTests
     {
+        /// <summary>
+        /// In this method while_iteration class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void while_iterationTest()
