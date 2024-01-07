13b13a895464b173d74c287fa93f90ed3840d74f
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:55:03 +0000

    XML documentation for method_calling class Unit Test

--- GDI+ Assignment ASETests/method_callingTests.cs
+++ GDI+ Assignment ASETests/method_callingTests.cs
@@ -8,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for method calling class which is used to call method that has no parametres in it.
+    /// </summary>
     [TestClass()]
     public class method_callingTests
     {
+
+        /// <summary>
+        /// In this method method_calling class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void cmdsTest()
