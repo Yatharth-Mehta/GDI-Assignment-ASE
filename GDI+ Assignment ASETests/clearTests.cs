9c819ae60195ca0598052e08520c270b101ecce8
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:27:25 +0000

    XML documentation of clear class Unit Test

--- GDI+ Assignment ASETests/clearTests.cs
+++ GDI+ Assignment ASETests/clearTests.cs
@@ -8,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for clear class which is used to circle the picture box when user
+    /// passes a valid clear command.
+    /// </summary>
     [TestClass()]
     public class clearTests
     {
+        /// <summary>
+        /// In this method the clear class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void drawTest()
