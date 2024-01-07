a6552206cbd112d3233f5519051a591e9f1848ca
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 05:08:28 +0000

    XML documentation of open button class Unit test

--- GDI+ Assignment ASETests/open_buttonTests.cs
+++ GDI+ Assignment ASETests/open_buttonTests.cs
@@ -8,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for open button class which is used to open files.
+    /// </summary>
     [TestClass()]
     public class open_buttonTests
     {
+
+        /// <summary>
+        /// In this method open button class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void openTest()
