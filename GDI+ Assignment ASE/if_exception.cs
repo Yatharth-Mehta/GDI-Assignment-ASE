22433e3ab2f04fbd2840707553ce7acfd75963f3
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 00:04:13 +0000

    Change in XML documentation of if_exception

--- GDI+ Assignment ASE/if_exception.cs
+++ GDI+ Assignment ASE/if_exception.cs
@@ -7,19 +7,19 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is an custom exception class for if command.
+    /// </summary>
     public class if_exception : Exception
     {
         /// <summary>
-        /// This is an custom exception class for if command.
+        /// This constructor will throw an exception by drawing an error string message on the canvas.
         /// </summary>
-        public if_exception(Graphics g,int ypos) 
+        /// <param name="g">Graphic element for drawing string on canvas</param>
+        /// <param name="ypos">The position of the text on Y-axis</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
+        public if_exception(Graphics g, int ypos)
         {
-            /// <summary>
-            /// This constructor will throw an exception by drawing an error string message on the canvas.
-            /// </summary>
-            /// <param name="g">Graphic element for drawing string on canvas</param>
-            /// <param name="ypos">The position of the text on Y-axis</param>
-            /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
             if (g is null)
             {
                 throw new ArgumentNullException(nameof(g));
