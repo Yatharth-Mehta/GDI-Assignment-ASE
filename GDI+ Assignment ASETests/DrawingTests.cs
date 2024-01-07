b8b1e7e6bba3018737d817736bc000d58a84181b
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:31:00 +0000

    XML documentation for Drawing class Unit Test

--- GDI+ Assignment ASETests/DrawingTests.cs
+++ GDI+ Assignment ASETests/DrawingTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Properties.Tests
 {
+    /// <summary>
+    /// This is a Unit test for drawing class which is used to draw whatever command user passes.
+    /// </summary>
     [TestClass()]
     public class DrawingTests
     {
+        ////// <summary>
+        /// In this method the drawing class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void drawTest()
