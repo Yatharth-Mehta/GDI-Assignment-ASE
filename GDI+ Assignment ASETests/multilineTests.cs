8557576149bf7c079a704851047dcea565a8d289
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 04:59:05 +0000

    XML documentation  for multiline class Unit test

--- GDI+ Assignment ASETests/multilineTests.cs
+++ GDI+ Assignment ASETests/multilineTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for multiline class which is used to handle multiline text box commands.
+    /// </summary>
     [TestClass()]
     public class multilineTests
     {
+        /// <summary>
+        /// In this method multiline class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void variablesTest()
