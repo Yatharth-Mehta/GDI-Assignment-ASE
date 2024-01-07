f817e78304b9528b00b61f5bb3834f628de0eeec
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 00:34:38 +0000

    XML documentation for reset_exception.cs

--- GDI+ Assignment ASE/reset_exception.cs
+++ GDI+ Assignment ASE/reset_exception.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE
 {
+    /// <summary>
+    /// This is an custom exception class for reset command.
+    /// </summary>
     public class reset_exception : Exception
     {
+        /// <param name="g">Graphic element for drawing string on canvas</param>
+        /// <param name="ypos">The position of the text on Y-axis</param>
+        /// <exception cref="ArgumentNullException">An exception to be thrown if the graphic variable is null</exception>
         public reset_exception(Graphics g,int ypos)
         {
             if (g is null)
