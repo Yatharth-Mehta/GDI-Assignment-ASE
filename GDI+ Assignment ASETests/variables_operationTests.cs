827a212b2d83167564b5b2a07111ee9e8e0a8578
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 05:24:54 +0000

    XML documentation for variable operation class Unit Test

--- GDI+ Assignment ASETests/variables_operationTests.cs
+++ GDI+ Assignment ASETests/variables_operationTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for variable operation class which is used to perform arithmetic opreations with the variables.
+    /// </summary>  
     [TestClass()]
     public class variables_operationTests
     {
+        /// <summary>
+        /// In this method variable_operation class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void variables_operationTest()
