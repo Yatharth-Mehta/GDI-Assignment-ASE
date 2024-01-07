a0fddbed0f39f05e447746f12949f3c8b7b9821f
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-07 05:27:06 +0000

    XML documentation for while_calling class Unit Test

--- GDI+ Assignment ASETests/while_callingTests.cs
+++ GDI+ Assignment ASETests/while_callingTests.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for while calling class which is used when using while loop to perform action .
+    /// </summary>
     [TestClass()]
     public class while_callingTests
     {
+        /// <summary>
+        /// In this method while_calling class is called and the Graphic object is passed null to create an exception.
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
         public void while_callingTest()
