849fea4040f7977f2cf0f5b8bdffd0a2076ae80a
Yathath Mehta <yatharthsmehtaleeds@gmail.com>
2024-01-08 12:30:21 +0000

    XML documentation for Syntax checking class Unit test

--- GDI+ Assignment ASETests/syntax_checkingTests.cs
+++ GDI+ Assignment ASETests/syntax_checkingTests.cs
@@ -8,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace GDI__Assignment_ASE.Tests
 {
+    /// <summary>
+    /// This is a Unit test for syntax checking class which is used to check the syntax of the commands passed
+    /// by the user
+    /// </summary>
     [TestClass()]
     public class syntax_checkingTests
     {
+        /// <summary>
+        /// This method will call the syntax checking class and will throw null to it's graphics element which will throw an error
+        /// </summary>
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException), "The Exception not found")]
 
